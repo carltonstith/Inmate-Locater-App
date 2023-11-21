fdd391cc4d0b0cd05596571fd80191f41eac679c
Carlton Stith <stith@temple.edu>
2023-11-21 15:39:23 -0500

    update app.js

--- app.js
+++ app.js
@@ -1,54 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose  = require('mongoose');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // Routes which should handle requests
-const inmateRoutes = require('./api/routes/inmates');
+const inmateRoutes = require("./api/routes/inmates");
 
 // Connect to MongoDB
 // process.env.MONGO_ATLAS_PW is a global variable set in nodemon.json => .env
-mongoose.connect('mongodb+srv://cstith:'+process.env.MONGODB_PW+'@inmatelocatorapp.hx0xnwg.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb+srv://cstith:" +
+    process.env.MONGODB_PW +
+    "@inmatelocatorapp.hx0xnwg.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 // Middleware
-app.use(morgan('dev')); // logs requests to the console
-app.use(bodyParser.urlencoded({extended: false})); // false because we don't want to parse extended bodies
+app.use(morgan("dev")); // logs requests to the console
+app.use(bodyParser.urlencoded({ extended: false })); // false because we don't want to parse extended bodies
 app.use(bodyParser.json()); // extract json data and make it readable
 
 // CORS error handling
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // allow access to any client
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // allow these headers
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); // allow these methods
-        return res.status(200).json({});
-    }
-    next();
+  res.header("Access-Control-Allow-Origin", "*"); // allow access to any client
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  ); // allow these headers
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET"); // allow these methods
+    return res.status(200).json({});
+  }
+  next();
 });
 
 // Routes which should handle requests
 //app.use('/inmates', inmateRoutes);
 
 // test route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // Error handling
 app.use((req, res, next) => {
-    const error = new Error('Not found'); // create new error
-    error.status = 404;
-    next(error); // pass error to next middleware
+  const error = new Error("Not found"); // create new error
+  error.status = 404;
+  next(error); // pass error to next middleware
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500); // set status code
-    res.json({
-        error: {
-            message: error.message // return error message
-        }
-    });
+  res.status(error.status || 500); // set status code
+  res.json({
+    error: {
+      message: error.message, // return error message
+    },
+  });
 }); // catch all errors
 
 module.exports = app;
